06fe9808923c58b6876b2204985981f1cf1ba9d6
xinyuG <xg897@nyu.edu>
2022-12-17 15:21:39 -0500

    2-7 add HTTP response handlinh test:test.http

--- src/main/java/com/xinyu/javafinalprojectxy/controller/TestController.java
+++ src/main/java/com/xinyu/javafinalprojectxy/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.xinyu.javafinalprojectxy.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //return string
 public class TestController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+    @PostMapping ("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post, " + name;
     }
 }
