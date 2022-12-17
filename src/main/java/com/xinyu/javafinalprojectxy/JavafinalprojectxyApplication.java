2b77d0b5dcb95124f6ff2b78150a0b124f8a770b
xinyuG <xg897@nyu.edu>
2022-12-16 22:28:37 -0500

    2-5 Revise start log

--- src/main/java/com/xinyu/javafinalprojectxy/JavafinalprojectxyApplication.java
+++ src/main/java/com/xinyu/javafinalprojectxy/JavafinalprojectxyApplication.java
@@ -1,13 +1,20 @@
 package com.xinyu.javafinalprojectxy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class JavafinalprojectxyApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(JavafinalprojectxyApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavafinalprojectxyApplication.class, args);
+		SpringApplication app = new SpringApplication(JavafinalprojectxyApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("Start!");
+		LOG.info("address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
