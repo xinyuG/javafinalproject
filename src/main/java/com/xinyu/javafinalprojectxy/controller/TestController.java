d5e02149fe436ab6d1541d6ccc9bc25e8e961412
xinyuG <xg897@nyu.edu>
2022-12-17 16:55:58 -0500

    2-9 add hot deployment

--- src/main/java/com/xinyu/javafinalprojectxy/controller/TestController.java
+++ src/main/java/com/xinyu/javafinalprojectxy/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.xinyu.javafinalprojectxy.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //return string
 public class TestController {
+    @Value("${test.hello}")         //${test.hello:TEST}, but config always has higher priority
+    private String testHello;
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
     @PostMapping ("/hello/post")
     public String helloPost(String name){
