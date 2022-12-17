394c3f440572e08f8b9e1ea9ff40fdb9b8021063
xinyuG <xg897@nyu.edu>
2022-12-16 19:59:27 -0500

    2-2 init

---
+++ src/main/java/com/xinyu/javafinalprojectxy/JavafinalprojectxyApplication.java
@@ -0,0 +1,13 @@
+package com.xinyu.javafinalprojectxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafinalprojectxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavafinalprojectxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xinyu/javafinalprojectxy/JavafinalprojectxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinyu.javafinalprojectxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafinalprojectxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
