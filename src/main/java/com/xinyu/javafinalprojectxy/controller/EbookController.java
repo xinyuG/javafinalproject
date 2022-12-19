7df4fadee7b0c1879bdf2d3dbec7cdf0e0ae5d07
xinyuG <xg897@nyu.edu>
2022-12-18 22:05:25 -0500

    3-6 add common return class CommonResp. All the back end return type is CommonResp

--- src/main/java/com/xinyu/javafinalprojectxy/controller/EbookController.java
+++ src/main/java/com/xinyu/javafinalprojectxy/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.xinyu.javafinalprojectxy.controller;
 
 import com.xinyu.javafinalprojectxy.domain.Ebook;
+import com.xinyu.javafinalprojectxy.resp.CommonResp;
 import com.xinyu.javafinalprojectxy.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,13 @@ public class EbookController {
     @Resource
     private EbookService ebookService;
     @GetMapping("/list")
-    public List<Ebook> list(){
+    /*response from back end*/
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
 
-        return ebookService.list();
+
     }
 }
